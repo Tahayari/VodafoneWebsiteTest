cd04232a980516fe7df7bb8662cf7ef501df77bb
dan.hosman <dan.hosman@vodafone.com>
2019-04-22 09:37:42 +0300

    Update micut pentru video pt3. Creare new branch

--- VodafoneWebsiteTest/src/main/java/ro/vodafone/qa/util/TestUtil.java
+++ VodafoneWebsiteTest/src/main/java/ro/vodafone/qa/util/TestUtil.java
@@ -1,8 +1,16 @@
 package ro.vodafone.qa.util;
 
-public class TestUtil {
+import ro.vodafone.qa.base.TestBase;
+
+public class TestUtil extends TestBase{
 	
 	public static long PAGE_LOAD_TIMEOUT = 30 ;
 	public static long IMPLICIT_WAIT = 10 ;
 
+//	public void switchToFrame(){
+//		driver.switchTo().frame("mainpanel");
+//	}
+//	
+	
+	
 }
--- VodafoneWebsiteTest/src/test/java/ro/vodafone/qa/testcases/LoginPageTest.java
+++ VodafoneWebsiteTest/src/test/java/ro/vodafone/qa/testcases/LoginPageTest.java
@@ -2,18 +2,19 @@ package ro.vodafone.qa.testcases;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import ro.vodafone.qa.base.TestBase;
 import ro.vodafone.qa.pages.LoginPage;
 import ro.vodafone.qa.pages.PostpaidDashboardPage;
+import ro.vodafone.qa.util.TestUtil;
 
 public class LoginPageTest extends TestBase	{
 	
 	LoginPage loginPage;
 	PostpaidDashboardPage postpaidDashboardPage;
+	TestUtil testUtil;
 	
 	public LoginPageTest(){
 		super();
@@ -23,19 +24,20 @@ public class LoginPageTest extends TestBase	{
 	public void setUp(){
 		initialization();
 		loginPage = new LoginPage();
+		testUtil = new TestUtil();
 	}
 	
-//	@Test(priority=1)
-//	public void loginPageTitleTest(){
-//		String title = loginPage.validateLoginPageTitle();
-//		Assert.assertEquals(title, "Inregistreaza-te in MyVodafone - Vodafone");
-//	}
-//	
-//	@Test(priority=2)
-//	public void vdfLogoTest(){
-//		boolean flag = loginPage.validateVDFLogo();
-//		Assert.assertTrue(flag);
-//	}
+	@Test(priority=1)
+	public void loginPageTitleTest(){
+		String title = loginPage.validateLoginPageTitle();
+		Assert.assertEquals(title, "Inregistreaza-te in MyVodafone - Vodafone");
+	}
+	
+	@Test(priority=2)
+	public void vdfLogoTest(){
+		boolean flag = loginPage.validateVDFLogo();
+		Assert.assertTrue(flag);
+	}
 	
 	@Test(priority=3)
 	public void loginMethodTest(){
