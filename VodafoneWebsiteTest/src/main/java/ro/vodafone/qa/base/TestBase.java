d958f9a4a4960cff5e7924e951c28ca61550b15e
dan.hosman <dan.hosman@vodafone.com>
2019-05-07 16:00:11 +0300

    Added WebEventListener class - log events

--- VodafoneWebsiteTest/src/main/java/ro/vodafone/qa/base/TestBase.java
+++ VodafoneWebsiteTest/src/main/java/ro/vodafone/qa/base/TestBase.java
@@ -11,13 +11,17 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import ro.vodafone.qa.util.TestUtil;
+import ro.vodafone.qa.util.WebEventListener;
 
 public class TestBase {
 
 	public static WebDriver driver;
 	public static Properties prop;
+	public static EventFiringWebDriver e_driver;
+	public static WebEventListener eventListener;
 
 
 	public TestBase(){
@@ -39,13 +43,20 @@ public class TestBase {
 	public static void initialization(){
 		String browserName = prop.getProperty("browser");
 		if(browserName.equalsIgnoreCase("chrome")){
-			WebDriverManager.chromedriver().version("73.0").setup();
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();			
 		}
 		else if (browserName.equalsIgnoreCase("firefox")){
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		}
+		
+		
+		e_driver = new EventFiringWebDriver(driver);
+		//Now create object of EventListnerHandler to register it with EventFiringWebDriver
+		eventListener = new WebEventListener();
+		e_driver.register(eventListener);
+		driver = e_driver;
 		
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
